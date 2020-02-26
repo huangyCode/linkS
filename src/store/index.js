ac5c6ec024d810cedda14824bc7a07b94aa894f7
fatekiki <fatekiki@gmail.com>
2020-02-26 21:34:58 +0800

    change

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import { createStore } from '@mpxjs/core'
 const store = createStore({
   state: {
-    foodList: [],
+    foodList: {},
     address: {}
   },
   mutations: {
     setMember(state, { list }) {
-      state.foodList = list
+      state.foodList = {...list}
     },
     clearMember(state) {
-      state.foodList = []
+      state.foodList = {}
     },
     delMember(state, { index }) {
       console.log(index)
